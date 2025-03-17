247f76e3184d5e25cd2d7632be60bb42e01f3cde
zenghedong <zenghedong@outlook.com>
2025-03-17 09:10:56 +0800

    修改platform_service的进程启动逻辑
    Signed-off-by: zenghedong <zenghedong@outlook.com>

--- platform_service.py
+++ platform_service.py
@@ -9,7 +9,6 @@
 @version    :1.0
 @Email      :2462491568@qq.com
 '''
-import multiprocessing.pool
 import os
 import multiprocessing
 from config import (redis_ip,
@@ -33,29 +32,6 @@ from ultralytics.utils import SettingsManager
 from services import trainService, exportService
 settings = SettingsManager()
 settings.update(datasets_dir="")
-
-
-def run_train():
-    train_service = trainService(redis_ip,
-                                 redis_port,
-                                 redis_pwd,
-                                 logs,
-                                 train_host_msg,
-                                 train_action_opt_topic_name,
-                                 train_action_result_topic_name,
-                                 train_data_download_topic_name)
-    train_service.run()
-
-
-def run_export():
-    export_service = exportService(redis_ip,
-                                   redis_port,
-                                   redis_pwd,
-                                   logs,
-                                   export_host_msg,
-                                   export_action_opt_topic_name,
-                                   export_action_result_topic_name)
-    export_service.run()
 
 
 def main():
