30da2330e1caf173a507c81f6cc9d9f56b158f68
zenghedong <zenghedong@outlook.com>
2025-06-16 10:43:53 +0800

    chore: 修改文件权限为可执行并添加启动脚本
    添加start.sh启动脚本用于同时运行jupyter notebook和训练服务
    将多个文件权限从644修改为755使其可执行
    
    Signed-off-by: zenghedong <zenghedong@outlook.com>

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+#启动jupyter
+echo "[startup] Starting Jupyter Notebook in /jupyter ..."
+jupyter notebook --allow-root &
+sleep 5
+#启动训练服务
+echo "[startup] Starting Train And Export Service ..."
+python platform_service.py &
+echo "[start.sh] Both Services are running, Enering wait ..."
+wait
